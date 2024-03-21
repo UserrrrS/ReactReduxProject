ef5bcffce6f0d5782586082dee74fcf977ae5b71
Artem <maksaretem1347@ya.ru>
2024-03-21 12:41:39 +0300

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import styles from '../../styles/Header.module.css'
+
+import { ROUTES } from '../../utils/routes'
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <div className={styles.logo}>
+        <Link to={ROUTES.HOME}>
+
+        </Link> 
+      </div>
+    </div>
+  )
+}
+
+export default Header
